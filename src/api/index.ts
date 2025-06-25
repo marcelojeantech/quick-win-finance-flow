d30b1596561e6db5f6237a3db31dd7f600fa0e1e
Marcelo Jean <marcelo.jean@brandlovrs.com>
2025-06-25 14:18:01 -0300

    initial 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,14 @@
 
 import axios from 'axios';
-// import env from 'config/env';
-// import { auth } from 'config/firebaseConfig';
 
 const api = axios.create({
 	baseURL: 'https://app.omie.com.br/api/v1',
 	headers: {
 		'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 	},
 });
-
-// curl -s https://app.omie.com.br/api/v1/financas/contareceber/ \
-//  -H 'Content-type: application/json' \
-//  -d '{"call":"ListarContasReceber","param":[{"pagina":1,"registros_por_pagina":100,"apenas_importado_api":"N"}],"app_key":"#APP_KEY#","app_secret":"#APP_SECRET#"}'
-
-
-// omie_api.interceptors.request.use(
-// 	async (config) => {
-//     // const idToken = await auth.currentUser?.getIdToken();
-//     // if (idToken) {
-//     //   config.headers['Authorization'] = `Bearer ${idToken}`;
-//     // }
-//     return config
-//   },
-// 	(error) => {
-// 		return Promise.reject(error);
-// 	}
-// );
 
 export default api;
